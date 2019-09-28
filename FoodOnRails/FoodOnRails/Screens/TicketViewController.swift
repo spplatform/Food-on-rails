ccba2b3c7732d1155c553090e5f5ceb5365a2f79
Kirill Klebanov <headmast@bk.ru>
2019-09-28 16:42:24 +0300

    Work with ticket number. Get rout request and perform it

--- FoodOnRails/FoodOnRails/Screens/TicketViewController.swift
+++ FoodOnRails/FoodOnRails/Screens/TicketViewController.swift
@@ -16,31 +16,47 @@ class TicketViewController: UIViewController {
     @IBOutlet var cameraBtn: UIButton!
     @IBOutlet var okBtn: UIButton!
     
+    var ticketId = ""
+    var routeRespone: RouteResponse?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        testRequest()
+        ticketId = "20073776662724"
     }
     
     @IBAction func handleScannerPresent(_ sender: Any, forEvent event: UIEvent) {
-      let viewController = makeBarcodeScannerViewController()
-      viewController.title = "Barcode Scanner"
-      present(viewController, animated: true, completion: nil)
+        let viewController = makeBarcodeScannerViewController()
+        viewController.title = "Barcode Scanner"
+        present(viewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func okPressed(_ sender: Any, forEvent event: UIEvent) {
+        routRequest(for: ticketId)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "openRoute" {
+            if let destinationVC = segue.destination as? RouteViewController {
+                destinationVC.routeRespone = routeRespone
+            }
+        }
     }
     
     private func makeBarcodeScannerViewController() -> BarcodeScannerViewController {
-      let viewController = BarcodeScannerViewController()
-      viewController.codeDelegate = self
-      viewController.errorDelegate = self
-      viewController.dismissalDelegate = self
-      return viewController
+        let viewController = BarcodeScannerViewController()
+        viewController.codeDelegate = self
+        viewController.errorDelegate = self
+        viewController.dismissalDelegate = self
+        return viewController
     }
 
     
-    func testRequest() {
-        RouteAPI.ticketIdRouteGet(id: "20073776662724") { (respose, error) in
+    func routRequest(for tickeitId: String) {
+        RouteAPI.ticketIdRouteGet(id: tickeitId) {[weak self] (respose, error) in
             print("request")
+            self?.routeRespone = respose
             print(respose?.trainNumber ?? "nan")
+            self?.performSegue(withIdentifier: "openRoute", sender: self)
         }
     }
 }
@@ -48,30 +64,27 @@ class TicketViewController: UIViewController {
 // MARK: - BarcodeScannerCodeDelegate
 
 extension TicketViewController: BarcodeScannerCodeDelegate {
-  func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
-    ticketNumber.text = code
-    print("Barcode Data: \(code)")
-    print("Symbology Type: \(type)")
-    controller.dismiss(animated: true, completion: nil)
-    //controller.reset()
-//    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-//      controller.reset()
-//    }
-  }
+    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
+        ticketId = code
+        ticketNumber.text = code
+        print("Barcode Data: \(code)")
+        print("Symbology Type: \(type)")
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
 
 // MARK: - BarcodeScannerErrorDelegate
 
 extension TicketViewController: BarcodeScannerErrorDelegate {
-  func scanner(_ controller: BarcodeScannerViewController, didReceiveError error: Error) {
-    print(error)
-  }
+    func scanner(_ controller: BarcodeScannerViewController, didReceiveError error: Error) {
+        print(error)
+    }
 }
 
 // MARK: - BarcodeScannerDismissalDelegate
 
 extension TicketViewController: BarcodeScannerDismissalDelegate {
-  func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
-    controller.dismiss(animated: true, completion: nil)
-  }
+    func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
